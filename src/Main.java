ea855d3714d2ff0b19d2531f146887757edcccd0
F3FFO <federico.pieranton@gmail.com>
2022-03-19 17:43:52 +0100

    Rename main class with java naming conventions

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import view.FrameClass;
 
 import javax.swing.*;
 
-public class mainClass {
+public class Main {
     public static void main(String[] args) {
         FlatDarkLaf.setup();
         UIManager.put("Button.arc", 13);
